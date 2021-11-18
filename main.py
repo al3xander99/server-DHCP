304c3cefcdb6eb8ef07e041cba7d1b3c7d381375
Alex Postudor <alexpostudor@gmail.com>
2021-11-18 22:31:28 +0200

    ?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import socket
 import threading
 import queue
-import time # why use this? # we have no 'time' to explain
+import time # why use this? # we have no 'time' to explain # i have to give you credit thats a good one
 
 if __name__ == '__main__':
     # serverDHCP.run()
