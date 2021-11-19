28165d625b563f458aaeb22f975587163dc01f84
Stefan <stef.lazar2000@gmail.com>
2021-11-19 08:22:16 +0200

    buna dimineata ora locala 820

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import threading
 import queue
 import time # why use this? # we have no 'time' to explain # i have to give you credit thats a good one #  cheers mate
 
+
+
+
+
 if __name__ == '__main__':
+    print('Hello world')
     # serverDHCP.run()
     # nu stiu daca o sa functioneze ce ai scris la line 9       -sl
