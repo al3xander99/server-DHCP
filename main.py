67bf4b5e8d90b6a3499207c1e26456365ce98d26
Stefan <stef.lazar2000@gmail.com>
2021-11-19 09:38:23 +0200

    proiect laborator 19/11

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from random import randint
 import os
-import socket
+import socket # pentru trimis pachete (cu udp) - folosind wireshark, receptionam pachetul
 import threading
 import queue
-import time # why use this? # we have no 'time' to explain # i have to give you credit thats a good one #  cheers mate
-import struct
+import time
+import struct # cu functia struct.pack() combinam octetii specifici unui mesaj dhcp intr-un pachet
+# vom creea clasa packet, cu campuri ce corespund fiecarui parametru din cadrul unui pachet. la final, vom "impacheta" acesti parametri intr-un singur sir de octeti cu functia struct.pack()
 
 packet = b''
-
-class DHCP_Discovery:
+#optiuni: tipul pachetului (D/O/R/A), sfarsitul pachetului (0xFF), masca de retea, adresa de gateway (comunicat in ext retelei), adresa de serere dns
+class DHCP_Offer:
     def __init__(self):
 
 
 if __name__ == '__main__':
     print('Hello world')
     # serverDHCP.run()
-    # nu stiu daca o sa functioneze ce ai scris la line 17       -sl
