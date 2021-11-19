84e7c736ffd42d99f2b2076dfc7341dbcb883393
Stefan <stef.lazar2000@gmail.com>
2021-11-19 08:45:48 +0200

    buna dimineata ora locala 845

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import random
+from random import randint
 import os
 import socket
 import threading
 import queue
 import time # why use this? # we have no 'time' to explain # i have to give you credit thats a good one #  cheers mate
+import struct
 
+packet = b''
 
-
+class DHCP_Discovery:
+    def __init__(self):
 
 
 if __name__ == '__main__':
     print('Hello world')
     # serverDHCP.run()
-    # nu stiu daca o sa functioneze ce ai scris la line 9       -sl
+    # nu stiu daca o sa functioneze ce ai scris la line 17       -sl
