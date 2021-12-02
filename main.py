fb28315099aaa7fc50eaef1216146512c0dde71e
Alex Postudor <alexpostudor@gmail.com>
2021-12-02 19:57:35 +0200

    thank you for coming to my ted talk

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from random import randint
 import os
-import socket # pentru trimis pachete (cu udp) - folosind wireshark, receptionam pachetul
+# pentru trimis pachete (cu udp) - folosind wireshark, receptionam pachetul
+import socket
 import threading
 import queue
 import time
-import struct # cu functia struct.pack() combinam octetii specifici unui mesaj dhcp intr-un pachet
+import struct  # cu functia struct.pack() combinam octetii specifici unui mesaj dhcp intr-un pachet
 # vom creea clasa packet, cu campuri ce corespund fiecarui parametru din cadrul unui pachet. la final, vom "impacheta" acesti parametri intr-un singur sir de octeti cu functia struct.pack()
 
 packet = b''
-#optiuni: tipul pachetului (D/O/R/A), sfarsitul pachetului (0xFF), masca de retea, adresa de gateway (comunicat in ext retelei), adresa de serere dns
+
+
+class packet():
+    pass
+
+# optiuni: tipul pachetului (D/O/R/A), sfarsitul pachetului (0xFF), masca de retea, adresa de gateway (comunicat in ext retelei), adresa de serere dns
+
+
 class DHCP_Offer:
     def __init__(self):
 
