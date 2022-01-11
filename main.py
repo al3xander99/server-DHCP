a2098b61c723b14ce68a98d50de05b36d7e81f0d
Alex Postudor <alexpostudor@gmail.com>
2022-01-11 22:36:31 +0200

    xd

--- main.py
+++ main.py
@@ -8,39 +8,36 @@ import time
 import struct  # cu functia struct.pack() combinam octetii specifici unui mesaj dhcp intr-un pachet
 # vom creea clasa packet, cu campuri ce corespund fiecarui parametru din cadrul unui pachet. la final, vom "impacheta" acesti parametri intr-un singur sir de octeti cu functia struct.pack()
 
-packet = b''
+#  class packet():
+#    def __init__(self, PAD, SUBNET_MASK, ROUTER, DOMAIN_SERVER,
+#                  HOST_NAME, BROADCAST_ADDRESS, ADDRESS_REQUEST,
+#                  LEASE_TIME, DHCP_MESSAGE_TYPE, RENEWAL_TIME, CLIENT_ID, END):
+
+#         self.PAD = PAD
+#         self.SUBNET_MASK = SUBNET_MASK
+#         self.ROUTER = ROUTER
+#         self.DOMAIN_SERVER = DOMAIN_SERVER
+#         self.HOST_NAME = HOST_NAME
+#         self.BROADCAST_ADDRESS = BROADCAST_ADDRESS
+#         self.ADDRESS_REQUEST = ADDRESS_REQUEST
+#         self.LEASE_TIME = LEASE_TIME
+#         self.DHCP_MESSAGE_TYPE = DHCP_MESSAGE_TYPE
+#         self.RENEWAL_TIME = RENEWAL_TIME
+#         self.CLIENT_ID = CLIENT_ID
+#         self.END = END
+
+#     def assembly(self, mesaj):
+#         self.PAD = 0
+#         self.SUBNET_MASK = (15 << 28) & mesaj
+#         self.ROUTER = (15 << 24) & mesaj
+#         self.DOMAIN_SERVER = (15 << 20) & mesaj
+#         self.HOST_NAME = (1 << 19) & mesaj
+#         self.BROADCAST_ADDRESS = (15 << 15) & mesaj
+#         self.ADDRESS_REQUEST = (15 << 14) & mesaj
+#         self.LEASE
 
 
-# class packet():
-   def __init__(self, PAD, SUBNET_MASK, ROUTER, DOMAIN_SERVER,
-                 HOST_NAME, BROADCAST_ADDRESS, ADDRESS_REQUEST,
-                 LEASE_TIME, DHCP_MESSAGE_TYPE, RENEWAL_TIME, CLIENT_ID, END):
-
-        self.PAD = PAD
-        self.SUBNET_MASK = SUBNET_MASK
-        self.ROUTER = ROUTER
-        self.DOMAIN_SERVER = DOMAIN_SERVER
-        self.HOST_NAME = HOST_NAME
-        self.BROADCAST_ADDRESS = BROADCAST_ADDRESS
-        self.ADDRESS_REQUEST = ADDRESS_REQUEST
-        self.LEASE_TIME = LEASE_TIME
-        self.DHCP_MESSAGE_TYPE = DHCP_MESSAGE_TYPE
-        self.RENEWAL_TIME = RENEWAL_TIME
-        self.CLIENT_ID = CLIENT_ID
-        self.END = END
-
-    def assembly(self, mesaj):
-        self.PAD = 0
-        self.SUBNET_MASK = (15 << 28) & mesaj
-        self.ROUTER = (15 << 24) & mesaj
-        self.DOMAIN_SERVER = (15 << 20) & mesaj
-        self.HOST_NAME = (1 << 19) & mesaj
-        self.BROADCAST_ADDRESS = (15 << 15) & mesaj
-        self.ADDRESS_REQUEST = (15 << 14) & mesaj
-        self.LEASE
-
-
-def DISCOVERY:
+def DISCOVERY():
     def __init__(self, data, transID):
         self.data = data
         self.transID = transID
