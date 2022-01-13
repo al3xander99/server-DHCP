606e899d4e5b31996bf2622cb7a231204c58e5ea
Alex Postudor <alexpostudor@gmail.com>
2022-01-13 22:09:17 +0200

    am facut si proiectul

--- main.py
+++ main.py
@@ -1,114 +1,33 @@
-from random import randint
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys
-from interfata_eleganta_2005 import Ui_MainWindow
-import os
-# pentru trimis pachete (cu udp) - folosind wireshark, receptionam pachetul
-import socket
+import interfata_eleganta_2005
+from Server import DHCP_server
+from queue import Queue
 import threading
-import queue
-import time
-import struct  # cu functia struct.pack() combinam octetii specifici unui mesaj dhcp intr-un pachet
-# vom creea clasa packet, cu campuri ce corespund fiecarui parametru din cadrul unui pachet. la final, vom "impacheta" acesti parametri intr-un singur sir de octeti cu functia struct.pack()
-
-packet = b''
 
 
-#  class packet():
-#    def __init__(self, PAD, SUBNET_MASK, ROUTER, DOMAIN_SERVER,
-#                  HOST_NAME, BROADCAST_ADDRESS, ADDRESS_REQUEST,
-#                  LEASE_TIME, DHCP_MESSAGE_TYPE, RENEWAL_TIME, CLIENT_ID, END):
-
-#         self.PAD = PAD
-#         self.SUBNET_MASK = SUBNET_MASK
-#         self.ROUTER = ROUTER
-#         self.DOMAIN_SERVER = DOMAIN_SERVER
-#         self.HOST_NAME = HOST_NAME
-#         self.BROADCAST_ADDRESS = BROADCAST_ADDRESS
-#         self.ADDRESS_REQUEST = ADDRESS_REQUEST
-#         self.LEASE_TIME = LEASE_TIME
-#         self.DHCP_MESSAGE_TYPE = DHCP_MESSAGE_TYPE
-#         self.RENEWAL_TIME = RENEWAL_TIME
-#         self.CLIENT_ID = CLIENT_ID
-#         self.END = END
-
-#     def assembly(self, mesaj):
-#         self.PAD = 0
-#         self.SUBNET_MASK = (15 << 28) & mesaj
-#         self.ROUTER = (15 << 24) & mesaj
-#         self.DOMAIN_SERVER = (15 << 20) & mesaj
-#         self.HOST_NAME = (1 << 19) & mesaj
-#         self.BROADCAST_ADDRESS = (15 << 15) & mesaj
-#         self.ADDRESS_REQUEST = (15 << 14) & mesaj
-#         self.LEASE
-
-
-def DISCOVERY():
-    def __init__(self, mesaj):
-        self.mesaj = mesaj
-        # self.transID = transID
-        self.SUBNET_MASK = ''
-        self.ROUTER = ''
-        self.DOMAIN_SERVER = ''
-
-        # self.BROADCAST_ADDRESS = '' (de pus in REQUEST?)
-        # self.ADDRESS_REQUEST = ''
-        # self.LEASE_TIME = ''
-        # self.DHCP_MESSAGE_TYPE = ''
-        # self.RENEWAL_TIME = ''
-        # self.CLIENT_ID = ''
-
-        self.unpack()
-
-    def unpack(self):  # mesaj[0:7] = 35 mesaj[8:15] = # of following bytes
-        self.SUBNET_MASK = self.mesaj[16:47]
-        self.ROUTER = self.mesaj[48:79]
-        self.DOMAIN_SERVER = self.mesaj[80:111]
-
-    #PAD = 0
-
-    # -------10 OPTIONS-------
-
-    #SUBNET_MASK = 1
-    #ROUTER = 3
-    #DOMAIN_SERVER = 6
-    #HOST_NAME = 12
-    #BROADCAST_ADDRESS = 28
-    #ADDRESS_REQUEST = 50
-    #LEASE_TIME = 51
-    #DHCP_MESSAGE_TYPE = 53
-    #RENEWAL_TIME = 58
-    #CLIENT_ID = 61
-
-    # ------------------------
-    #END = 255
-    pass
-
-# optiuni: tipul pachetului (D/O/R/A), sfarsitul pachetului (0xFF), masca de retea, adresa de gateway (comunicat in ext retelei), adresa de serere dns
-
-
-def Offer():
-    def __init__(self, mesaj):
-        pass
-
-
-def Request():
-    def __init__(self, mesaj):
-        pass
-
-
-def Ack():
-    def __init__(self, mesaj):
-        pass
-
-
-if __name__ == '__main__':
-    print('Hello world')
+def func(out_q):
+    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = interfata_eleganta_2005.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    ui.completariStatice()
+
+    def mor():
+        data = out_q.get()
+        if(data):
+            ui.adaugaText(data)
+        else:
+            pass
+    timer = QtCore.QTimer()
+    timer.timeout.connect(mor)
+    timer.start(2000)
     sys.exit(app.exec_())
-    # serverDHCP.run()
-    # am stricat tot?
+
+
+if __name__ == "__main__":
+    q = Queue()
+    dhcp_server = DHCP_server()
+    x = threading.Thread(target=dhcp_server.server, args=(q,)).start()
+    y = threading.Thread(target=func, args=(q,)).start()
